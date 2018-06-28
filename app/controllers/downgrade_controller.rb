6ff9105e076ab340409ad8b637e777fddd57e1d9
Sven Larson <svenlarson1098@gmail.com>
2018-06-28 21:09:01 +0000

    Private Wikis assignment complete

--- app/controllers/downgrade_controller.rb
+++ app/controllers/downgrade_controller.rb
@@ -4,11 +4,11 @@ class DowngradeController < ApplicationController
   
   def update
     current_user.standard!
-   # current_user.wikis.each do |pub|
-  #    pub.update_attribute(:private, false)
-   # end
+    current_user.wikis.each do |pub|
+    pub.update_attribute(:private, false)
+  end
  
-    flash[:notice] = "Your membership has been downgraded to a standard account."
+    flash[:success] = "Your membership has been downgraded to a standard account. All private wikis are now public."
       redirect_to root_path
   end
 end
